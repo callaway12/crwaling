bdda08e8add272208c629714ef38c8fc3e913c6f
callaway12 <taewankim0925@likelion.org>
2018-12-13 21:26:22 +0900

    201812132126
    노코기리로 네이버 검색 페이지에서 주소랑 그런거 다 가져옴 ㅇㅇㅇㅇ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,19 @@
 require 'open-uri'
 class HomeController < ApplicationController
     def crawler
-        
 
-            url = "https://blog.naver.com/mardukas/221405729725"
+
+            url = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EC%8A%A4%EC%8B%9C+%EB%A0%A4&oquery=%EB%85%B8%EC%BD%94%EA%B8%B0%EB%A6%AC+%ED%81%AC%EB%A1%A4%EB%A7%81&tqi=Ut42swpySEKssvkdf1GssssssB8-077335"
             doc = Nokogiri::HTML(open(url, 'User-Agent' => 'chrome')) # 열고
-            @post = doc.css("p[class = 'se_textarea']")
+            @post = doc.css("div [id = 'sp_local_1']")
             @post.each do |post|
-             tit = post.text
+             tit = post.css("strong").text.strip
+              # content = post.css("span")[0].text.strip 이게 뭐 파는지
+              content = post.css("dd")[0].text.strip.split('<br>').slice(0)
+              #dd tag [0] 은 주소 [1] 전화번호 [2] 운영시간 [3] 가격대
 
 
-             @res = Result.new(name: tit)
+             @res = Result.new(name: tit, content: content)
              @res.save
              @pr = Result.all
         end
@@ -36,9 +39,9 @@ end
 #                 addPage = "page#{c}/"
 #             end
 #             url = "https://search.shopping.naver.com/search/all.nhn?origQuery=%EC%98%81%EC%96%91%EC%A0%9C&pagingIndex=" + addPage # url을 지정해
-            
+
 #             doc = Nokogiri::HTML(open(url)) # 열고
-#             @posts = doc.css('.goodslist ad _itemSection') 
+#             @posts = doc.css('.goodslist ad _itemSection')
 #             @posts.each do |x| #각각 돌면서 Result에 추가해줍니다.
 #                 tit = x.css('.tit').text.strip
 #                 cont = x.css('.num _price_reload').integer.strip
